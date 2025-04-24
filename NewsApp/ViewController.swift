1717317a7c753cc604c1b3fa5aec6d4e50c8b75c
abhishek2355 <abhishekraut2355@gmail.com>
2025-04-24 23:33:04 +0530

    Initial Setup

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Abhishek Raut on 24/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
